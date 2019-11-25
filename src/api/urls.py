8859dae35aafc28d1fb8fd53591d23ca95773e51
rijkerd <richardaggrey7@gmail.com>
2019-11-25 19:41:09 +0300

    refactor: add url and views for song and album models

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import include, path
 from rest_framework.schemas import get_schema_view
-from music.views import MusicListView
+from music import views
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
-    path('songs/', MusicListView.as_view({'get': 'list'})),
+    path('songs/', views.SongList.as_view(), name='song-list'),
+    path('song/<int:pk>', views.SongDetail.as_view(), name='song-detail'),
+    path('albums/', views.AlbumList.as_view(), name='album-list'),
+    path('album/<int:pk>', views.AlbumDetails.as_view(), name='album-detail'),
+    # path('songs/', MusicListView.as_view({'get': 'list'})),
+    # path('album/', AlbumListView.as_view({'get': 'list'})),
 ]
--- src/music/views.py
+++ src/music/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
-from music.models import Song
-from music.serializers import SongSerializer
+from .models import Song, Album
+from .serializers import SongSerializer, AlbumSerializer
 
 
-class MusicListView(viewsets.ReadOnlyModelViewSet):
+class SongList(ListCreateAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+
+
+class SongDetail(RetrieveUpdateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
+class AlbumList(ListCreateAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+
+class AlbumDetails(RetrieveUpdateAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+# class GenreDetails
+
+# class MusicListView(viewsets.ReadOnlyModelViewSet):
+#     queryset = Song.objects.all()
+#     serializer_class = SongSerializer
+
+
+# class AlbumListView(viewsets.ReadOnlyModelViewSet):
+#     queryset = Album.objects.all()
+#     serializer_class = AlbumSerializer
