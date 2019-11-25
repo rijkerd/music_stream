fbc3d58f8d1a664ec2a6a3ff752a7daa9ef3c934
rijkerd <richardaggrey7@gmail.com>
2019-11-25 19:40:26 +0300

    refactor: serializer linked models

--- src/music/serializers.py
+++ src/music/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models import Song
+from .models import Song, Album, Genre
+from users.models import CustomUser
+from artist.serializers import ArtistSerializers
 
 
-class SongSerializer(serializers.ModelSerializer):
+class GenreSerializer(serializers.ModelSerializer):
+    pass
+
+
+class SongSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.ReadOnlyField(source='CustomUser.email')
+    album_name = serializers.HyperlinkedIdentityField(
+        view_name='album-detail', format='json')
+
     class Meta:
         model = Song
-        fields = '__all__'
+        fields = ['user', 'title',
+                  'song_url', 'song_thumbnail', 'album_name', 'created_on']
+
+
+class AlbumSerializer(serializers.HyperlinkedModelSerializer):
+    songs = SongSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Album
+        fields = ['id', 'album_name', 'album_thumbnail', 'songs', 'created_on']
