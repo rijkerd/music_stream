7629253039af5dc00e4eace0470151fb20a9525b
rijkerd <richardaggrey7@gmail.com>
2019-11-26 17:03:00 +0300

    refactor: clean up api module

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,16 +1,10 @@
 from django.urls import include, path
 from rest_framework.schemas import get_schema_view
-from music import views
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
-    path('songs/', views.SongList.as_view(), name='song-list'),
-    path('song/<int:pk>', views.SongDetail.as_view(), name='song-detail'),
-    path('albums/', views.AlbumList.as_view(), name='album-list'),
-    path('album/<int:pk>', views.AlbumDetails.as_view(), name='album-detail'),
-    # path('songs/', MusicListView.as_view({'get': 'list'})),
-    # path('album/', AlbumListView.as_view({'get': 'list'})),
+    path('', include('music.urls'))
 ]
