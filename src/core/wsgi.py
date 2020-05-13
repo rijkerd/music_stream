bea76a99ccb7abf3714e703fa7e75df8820088d5
rijkerd <richardaggrey7@gmail.com>
2020-05-13 13:54:05 +0300

    fix: set path for static files

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -15,5 +15,5 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.core.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+application = WhiteNoise(application, root='src/static')
 # application = DjangoWhiteNoise(application)
