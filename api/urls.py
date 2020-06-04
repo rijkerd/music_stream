3afd44ca4020f4fd102a4c71a58661229f267305
rijkerd <richardaggrey7@gmail.com>
2020-06-04 23:49:38 +0300

    chore: add bulma css

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
 from rest_framework.schemas import get_schema_view
+from django.views.generic.base import TemplateView
+
+
+class TestTemplate(TemplateView):
+    template_name = "home.html"
+
 
 urlpatterns = [
+    path('', TestTemplate.as_view()),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api-auth/', include('rest_framework.urls')),
