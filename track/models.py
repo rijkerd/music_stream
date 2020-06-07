3dd854cc65ea0cef9ac6abd5803186d368bcf5ee
rijkerd <richardaggrey7@gmail.com>
2020-06-07 15:36:12 +0300

    refactor: setup track app

---
+++ track/models.py
@@ -0,0 +1,33 @@
+# import uuid
+from django.db import models
+# from users.models import CustomUser
+# from artist.models import Artist
+
+
+class Track(models.Model):
+    pass
+    # id = models.UUIDField(
+    #     primary_key=True, default=uuid.uuid4, editable=False)
+    # owner = models.ForeignKey(
+    #     CustomUser, related_name='tracks', on_delete=models.CASCADE)
+    # artist = models.ForeignKey(
+    #     Artist, related_name='tracks', on_delete=models.CASCADE)
+    # title = models.CharField(
+    #     max_length=200, blank=False, default='Title')
+    # description = models.CharField(max_length=200,
+    #                                default="", blank=False)
+    # genre = models.ForeignKey(
+    #     Genre, related_name='tracks', on_delete=models.PROTECT)
+    # album = models.ForeignKey(
+    #     Album, related_name='songs', on_delete=models.CASCADE)
+    # song_thumbnail = models.ImageField(upload_to='songsImages')
+    # song_url = models.FileField(upload_to='music')
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    # class Meta:
+    #     unique_together = ['album', 'created_at']
+    #     ordering = ['created_at']
+
+    # def __str__(self):
+    #     return self.title
