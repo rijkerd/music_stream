e9734a0b0c4f4c6ca726bfd29202a578852121b3
rijkerd <richardaggrey7@gmail.com>
2020-06-07 15:50:14 +0300

    fix: implement tracks model and serializer

--- track/models.py
+++ track/models.py
@@ -1,33 +1,35 @@
-# import uuid
+import uuid
 from django.db import models
-# from users.models import CustomUser
-# from artist.models import Artist
+from users.models import CustomUser
+from artist.models import Artist
+from genre.models import Genre
+from album.models import Album
 
 
 class Track(models.Model):
     pass
-    # id = models.UUIDField(
-    #     primary_key=True, default=uuid.uuid4, editable=False)
-    # owner = models.ForeignKey(
-    #     CustomUser, related_name='tracks', on_delete=models.CASCADE)
-    # artist = models.ForeignKey(
-    #     Artist, related_name='tracks', on_delete=models.CASCADE)
-    # title = models.CharField(
-    #     max_length=200, blank=False, default='Title')
-    # description = models.CharField(max_length=200,
-    #                                default="", blank=False)
-    # genre = models.ForeignKey(
-    #     Genre, related_name='tracks', on_delete=models.PROTECT)
-    # album = models.ForeignKey(
-    #     Album, related_name='songs', on_delete=models.CASCADE)
-    # song_thumbnail = models.ImageField(upload_to='songsImages')
-    # song_url = models.FileField(upload_to='music')
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(
+        CustomUser, related_name='tracks', on_delete=models.CASCADE)
+    artist = models.ForeignKey(
+        Artist, related_name='tracks', on_delete=models.CASCADE)
+    title = models.CharField(
+        max_length=200, blank=False, default='Title')
+    description = models.CharField(max_length=200,
+                                   default="", blank=False)
+    genre = models.ForeignKey(
+        Genre, related_name='tracks', on_delete=models.PROTECT)
+    album = models.ForeignKey(
+        Album, related_name='tracks', on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='tracks/images')
+    url = models.FileField(upload_to='music')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    # class Meta:
-    #     unique_together = ['album', 'created_at']
-    #     ordering = ['created_at']
+    class Meta:
+        unique_together = ['album', 'created_at']
+        ordering = ['created_at']
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
---
+++ track/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Track
+from users.models import CustomUser
+from artist.serializers import ArtistSerializer
+
+
+class TrackSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field='name')
+    album = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field='name')
+    artist = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field='name')
+    owner = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field='username')
+    genre = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field='name')
+
+    class Meta:
+        model = Track
+        fields = "__all__"
